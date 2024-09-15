1a12e6195abd97b6d3f184bba0230e2a7a58c30e
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-16 02:00:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,10 @@ require("dotenv").config();
 const User = require("./model/registerModel");
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-app.use(cors({
-  origin: ['https://trailer-time.vercel.app'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true 
-}));
+app.use(cors({ origin: 'https://trailer-time.vercel.app' }))
 
 // Connect to MongoDB
 mongoose
