759770348c4fe26ed00811ad75f22fb9605af0a9
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-19 15:30:18 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, require: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   resetPasswordOTP: { type: Number },
-  resetPasswordExpires: { type: Date }
+  resetPasswordExpires: { type: Date },
 });
 
 module.exports = mongoose.model("User", userSchema);
